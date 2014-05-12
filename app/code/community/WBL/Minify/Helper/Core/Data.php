c29bd8bf0230c50fdfbb49c641505fdbc7d58e5b
Cyrill Schumacher <cyrill@schumacher.fm>
2014-05-12 12:30:48 +1000

    Return as early

--- app/code/community/WBL/Minify/Helper/Core/Data.php
+++ app/code/community/WBL/Minify/Helper/Core/Data.php
@@ -153,59 +153,62 @@ class WBL_Minify_Helper_Core_Data extends Mage_Core_Helper_Data
                 }
             }
 
-            // merge contents into the file
-            if ($shouldMerge) {
-                if ($targetFile && !is_writeable(dirname($targetFile))) {
-                    // no translation intentionally
-                    throw new Exception(sprintf('Path %s is not writeable.', dirname($targetFile)));
-                }
+            // no need to merge
+            if (false === $shouldMerge) {
+                return true;
+            }
 
-                // filter by extensions
-                if ($extensionsFilter) {
-                    if ($extensionsFilter == 'css') {
-                        $extensionsFilter = array('css');
-                    }
-                    if (!is_array($extensionsFilter)) {
-                        $extensionsFilter = array($extensionsFilter);
-                    }
-                    if (!empty($srcFiles)) {
-                        foreach ($srcFiles as $key => $file) {
-                            $fileExt = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-                            if (!in_array($fileExt, $extensionsFilter)) {
-                                unset($srcFiles[$key]);
-                            }
+            // merge contents into the file
+            if ($targetFile && !is_writeable(dirname($targetFile))) {
+                // no translation intentionally
+                throw new Exception(sprintf('Path %s is not writeable.', dirname($targetFile)));
+            }
+
+            // filter by extensions
+            if ($extensionsFilter) {
+                if ($extensionsFilter == 'css') {
+                    $extensionsFilter = array('css');
+                }
+                if (!is_array($extensionsFilter)) {
+                    $extensionsFilter = array($extensionsFilter);
+                }
+                if (!empty($srcFiles)) {
+                    foreach ($srcFiles as $key => $file) {
+                        $fileExt = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+                        if (!in_array($fileExt, $extensionsFilter)) {
+                            unset($srcFiles[$key]);
                         }
                     }
                 }
-                if (empty($srcFiles)) {
-                    // no translation intentionally
-                    throw new Exception('No files to compile.');
-                }
+            }
+            if (empty($srcFiles)) {
+                // no translation intentionally
+                throw new Exception('No files to compile.');
+            }
 
-                $data = '';
-                foreach ($srcFiles as $file) {
-                    if (!file_exists($file)) {
-                        continue;
-                    }
-                    $contents = file_get_contents($file) . "\n";
-                    if ($beforeMergeCallback && is_callable($beforeMergeCallback)) {
-                        $contents = call_user_func($beforeMergeCallback, $file, $contents);
-                    }
-                    $data .= $contents;
+            $data = '';
+            foreach ($srcFiles as $file) {
+                if (!file_exists($file)) {
+                    continue;
                 }
-                if (!$data) {
-                    // no translation intentionally
-                    throw new Exception(sprintf("No content found in files:\n%s", implode("\n", $srcFiles)));
+                $contents = file_get_contents($file) . "\n";
+                if ($beforeMergeCallback && is_callable($beforeMergeCallback)) {
+                    $contents = call_user_func($beforeMergeCallback, $file, $contents);
                 }
-                if ($targetFile) {
+                $data .= $contents;
+            }
+            if (!$data) {
+                // no translation intentionally
+                throw new Exception(sprintf("No content found in files:\n%s", implode("\n", $srcFiles)));
+            }
+            if ($targetFile) {
 
-                    //only the following line has been added for WBL_Minify
-                    $data = $this->minifyJsCss($data, $targetFile);
+                //only the following line has been added for WBL_Minify
+                $data = $this->minifyJsCss($data, $targetFile);
 
-                    file_put_contents($targetFile, $data, LOCK_EX);
-                } else {
-                    return $data; // no need to write to file, just return data
-                }
+                file_put_contents($targetFile, $data, LOCK_EX);
+            } else {
+                return $data; // no need to write to file, just return data
             }
 
             return true; // no need in merger or merged into file successfully
